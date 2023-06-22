56aac8745f0325f4b3f9e05529db6a2b8d917764
guybanbo <116914829+guybanbo@users.noreply.github.com>
2023-06-22 23:39:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,20 +2,28 @@ const express = require("express");
 const db = require("./db");
 
 const Pizza = require("./models/pizzaModel");
-
+const path = require('path')
 const ProductRouts = require("./routes/productsRouts");
 const userRoute = require("./routes/userRoute");
 const ordersRoute = require("./routes/ordersRoute");
 
 const app = express();
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("Hello World!!");
-});
+
 
 app.use("/api/pizzas/", ProductRouts);
 app.use("/api/users/", userRoute);
 app.use("/api/orders/", ordersRoute);
+if(process.env.NODE_ENV ==='production')
+{
+    app.use('/' , express.static('client/build'))
+
+    app.get('*' , (req , res)=>{
+
+        res.sendFile(path.resolve(__dirname  , 'client/build/index.html'))
+
+    })
+}
 
 const port = process.env.PORT || 5050;
 
