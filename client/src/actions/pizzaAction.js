59a5fbf772acc4e828f5d9bcb34d214dbecb58b8
guybanbo <116914829+guybanbo@users.noreply.github.com>
2023-06-24 19:55:38 +0300

    Update pizzaAction.js
    add .data to response.data

--- client/src/actions/pizzaAction.js
+++ client/src/actions/pizzaAction.js
@@ -5,7 +5,7 @@ export const getAllProducts = () => async (dispatch) => {
   try {
     const response = await axios.get("/api/pizzas/getallpizzas");
 
-    dispatch({ type: "GET_ALL_PRODUCTS_SUCCESS", payload: response.data });
+    dispatch({ type: "GET_ALL_PRODUCTS_SUCCESS", payload: response.data.data });
   } catch (error) {
     dispatch({ type: "GET_ALL_PRODUCTS_FAILURE", payload: error });
 
