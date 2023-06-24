6fd98bcc87ce0033dee205c4f22a7f2f1bdf42ce
guybanbo <116914829+guybanbo@users.noreply.github.com>
2023-06-24 19:44:13 +0300

    Update pizzaAction.js

--- client/src/actions/pizzaAction.js
+++ client/src/actions/pizzaAction.js
@@ -3,7 +3,7 @@ export const getAllProducts = () => async (dispatch) => {
   dispatch({ type: "GET_ALL_PRODUCTS_REQUEST" });
 
   try {
-    const response = await axios.get("http://pizza-braude-server.vercel.app"+"/api/pizzas/getallpizzas");
+    const response = await axios.get("/api/pizzas/getallpizzas");
 
     dispatch({ type: "GET_ALL_PRODUCTS_SUCCESS", payload: response.data });
   } catch (error) {
