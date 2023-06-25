2b97b92b492256ad2bd463195f85fade5e13fd78
guybanbo <116914829+guybanbo@users.noreply.github.com>
2023-06-25 17:50:31 +0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -6,11 +6,11 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./store";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
+  document.getElementById('root')
 );
 
 reportWebVitals();
